61b5753a4285060a3ead1e52e1e8f0afa7de5d83
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-23 21:04:30 -0700

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    // deklaracija clanova klase
+    public static int i;
+    public int j;
+    
+    // deklaracija metoda klase
+    public void prikazi()
+    {
+        Console.WriteLine("i={0} j={1}", i, j);
+
+    }
+}
+class prog1
+{
+    public static void Main()
+    {
+        Program rc1 = new Program();
+        rc1.j = 1;
+        Program rc2 = new Program();
+        rc2.j = 2;
+        Program.i = 3;
+
+        rc1.prikazi();
+        rc2.prikazi();
+    }
+}
